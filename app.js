802658e644d7b6781830b6fbc69a876ffb685601
Asilbek Asilov <asil.california@gmail.com>
2022-10-22 23:51:40 -0700

    Get api from unsplash

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const access_key = 'tjhU1W53pUTud8SOKjhtl8Mmdr9rr1fTjSRUXIpQFA0';
 
+const random_photo_url = `https://api.unsplash.com//photos/random?client_id=${access_key}&count=30`;
+
+const gallery = document.querySelector('.gallery');
+
+let allImages; // this will store all the images
+
+const getImages = () => {
+    fetch(random_photo_url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
