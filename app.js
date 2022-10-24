daea6eb13ad109de0394648d23f50f815b032ce2
Asilbek Asilov <asil.california@gmail.com>
2022-10-23 21:46:47 -0700

    Get image API and append to DOM

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const access_key = 'tjhU1W53pUTud8SOKjhtl8Mmdr9rr1fTjSRUXIpQFA0';
 
-const random_photo_url = `https://api.unsplash.com//photos/random?client_id=${access_key}&count=30`;
+const random_photo_url = `https://api.unsplash.com//photos/random?client_id=${access_key}&count=20`;
 
 const gallery = document.querySelector('.gallery');
 
@@ -9,5 +9,21 @@ let allImages; // this will store all the images
 const getImages = () => {
     fetch(random_photo_url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        allImages = data;
+        makeImages(allImages);
+    })
 }
+
+const makeImages = (data) => {
+    data.forEach((item, index) => {
+        
+        let img = document.createElement('img');
+        img.src = item.urls.regular;
+        img.className = 'gallery-img';
+
+        gallery.appendChild(img);
+    })
+}
+
+getImages()
